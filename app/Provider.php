eeddf67dec2d7be232d458c44821c14facbe1203
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-01-03 18:06:03 -0400

    CRUD completo de Productos

--- app/Provider.php
+++ app/Provider.php
@@ -9,4 +9,8 @@ class Provider extends Model
     protected $fillable = [
         'name', 'email', 'ruc_number', 'address', 'phone',
     ];
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
