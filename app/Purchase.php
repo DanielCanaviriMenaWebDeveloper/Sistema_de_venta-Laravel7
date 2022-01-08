6480acaee0e03fe37a6b78e43ca100c89d5c86c4
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-01-07 22:40:39 -0400

    Trabajando el CRUD de Ventas

--- app/Purchase.php
+++ app/Purchase.php
@@ -15,4 +15,16 @@ class Purchase extends Model
         'status',
         'picture'
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function provider() {
+        return $this->belongsTo(Provider::class);
+    }
+
+    public function purchaseDetails() {
+        return $this->hasMany(PurchaseDetails::class);
+    }
 }
--- app/PurchaseDetails.php
+++ app/PurchaseDetails.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseDetails extends Model
 {
-    //
+    protected $fillable = [
+        'shopping_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    public function purchase() {
+        return $this->belongsTo(Purchase::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
